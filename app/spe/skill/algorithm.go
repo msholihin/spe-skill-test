9b479964858c21a4fd92f37235f961cb387893ea
Muhammad Sholihin <muh.sholihins@gmail.com>
2023-03-08 14:24:59 +0700

    needle in haystack

--- app/spe/skill/algorithm.go
+++ app/spe/skill/algorithm.go
@@ -45,6 +45,15 @@ func (s *SpeSkillTest) FindOutlier(arr []int) interface{} {
 	return false // no outlier found
 }
 
+func (s *SpeSkillTest) FindNeedle(haystack []string, needle string) int {
+	for i, str := range haystack {
+		if str == needle {
+			return i
+		}
+	}
+	return -1 // needle not found
+}
+
 func main() {
 	test := SpeSkillTest{}
 
@@ -56,4 +65,7 @@ func main() {
 	fmt.Println(test.FindOutlier([]int{2, 4, 0, 100, 4, 11, 2602, 36})) // 11
 	fmt.Println(test.FindOutlier([]int{160, 3, 1719, 19, 11, 13, -21})) // 160
 	fmt.Println(test.FindOutlier([]int{11, 13, 15, 19, 9, 13, -21}))    // false
+
+	//NEEDLE IN THE HAYSTACK
+	fmt.Println(test.FindNeedle([]string{"red", "blue", "yellow", "black", "grey"}, "blue")) // 1
 }
